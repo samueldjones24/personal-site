ef3c61a15c08a9048deebdcc9f503eb6200dd340
Sam Jones <samueldjones24@gmail.com>
2022-02-24 14:05:13 +0000

    Meta image

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     const meta = {
       title: 'Samuel Jones - Software Developer',
       description:
-        'A semi-interesting dev blog about JavaScript, CSS and HTML, and other fun topics.',
+        'A semi-interesting tech blog about JavaScript, CSS and HTML.',
       image:
         'https://s3.eu-west-2.amazonaws.com/samjones.codes/Screenshot+2021-08-25+at+12.21.48.png'
     }
@@ -15,10 +15,14 @@ class MyDocument extends Document {
         <Head>
           <meta name="robots" content="follow, index" />
           <meta name="description" content={meta.description} />
-          <meta property="og:site_name" content={meta.title} />
-          <meta property="og:description" content={meta.description} />
-          <meta property="og:title" content={meta.title} />
-          <meta property="og:image" content={meta.image} />
+          <meta name="site_name" property="og:site_name" content={meta.title} />
+          <meta
+            name="description"
+            property="og:description"
+            content={meta.description}
+          />
+          <meta name="title" property="og:title" content={meta.title} />
+          <meta name="image" property="og:image" content={meta.image} />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@samueldjones" />
           <meta name="twitter:title" content={meta.title} />
